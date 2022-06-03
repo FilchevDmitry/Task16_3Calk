3ea2502516e745d7aac1dbfa3e120d5e6a707e98
DmityFilchev <filchev_dmitry@mail.ru>
2022-06-03 14:17:24 +0300

    Добавьте файлы проекта.

---
+++ Task16_3Calk/Task16_3Calk.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+int main()
+{	
+	string temp;
+	cin >> temp;
+	stringstream text(temp);
+	string a;
+	string b;
+	string c;
+	text >> a >> b >> c ;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << text.str();
+
+}
