657be0f450310673583e19f3e0fc4f6709c39f6f
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-06-05 08:29:00 +0300

    'ok'

--- Task16_3Calk/Task16_3Calk.cpp
+++ Task16_3Calk/Task16_3Calk.cpp
@@ -6,15 +6,32 @@ using namespace std;
 int main()
 {	
 	string temp;
+	cout << "Enter the expression (+, / , * , -) : ";
 	cin >> temp;
 	stringstream text(temp);
-	string a;
-	string b;
-	string c;
+	double a;
+	char b;
+	double c;
+	double result = 0;
 	text >> a >> b >> c ;
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
-	cout << text.str();
-
+	switch (b)
+	{
+	case '+':
+		result = a + c;
+		break;
+	case'-':
+		result = a - c;
+		break;
+	case '*':
+		result = a * c;
+		break;
+	case '/':
+		result = a / c;
+		break;
+	default:
+		cout << "Wrong!! Incorrect expression entered ! " << endl;
+		break;
+	}
+	temp = to_string(result);
+	cout <<"Calculation result = " << temp << endl;
 }
